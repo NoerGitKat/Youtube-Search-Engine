83ed4f0ded8719407a990935a0c1d5db8b040d02
Noer <noerpaanakker@hotmail.com>
2017-06-18 15:48:58 +0200

    styled background and thumbnails, almost finished search functionality displayed in list

--- js/script.js
+++ js/script.js
@@ -33,27 +33,59 @@ $(document).ready(() => {
 });
 
 function search() {
-		// Clear Results
-		$('#results').html('');
-		$('#buttons').html('');
+	// Clear Results
+	$('#results').html('');
+	$('#buttons').html('');
 
-		//Get Form Input
-		q = $('#query').val();
+	//Get Form Input
+	q = $('#query').val();
 
-		//Run GET Request on API
-		$.get(
-			'https://www.googleapis.com/youtube/v3/search', {
-			part: 'snippet, id',
-			q: q,
-			type: 'video',
-			key: 'AIzaSyDNWZzft1jXR2MVPHoEvUn2wVGzlfHkRTE'
-		},
-			function(data) {
-				var nextPageToken = data.nextPageToken;
-				var prevPageToken = data.prevPageToken;
+	//Run GET Request on API
+	$.get(
+		'https://www.googleapis.com/youtube/v3/search', {
+		part: 'snippet, id',
+		q: q,
+		type: 'video',
+		key: 'AIzaSyDNWZzft1jXR2MVPHoEvUn2wVGzlfHkRTE'
+	},
+		function(data) {
+			var nextPageToken = data.nextPageToken;
+			var prevPageToken = data.prevPageToken;
 
-				console.log(data);
+			// Log Data to Make Sure It Works
+			console.log(data);
 
-				// $.each(data.items) {}
+			$.each(data.items, function(i, item) {
+				// Get Output of Each Loop
+				var output = getOutput(item);
+
+
+				//Display Search Results
+				$('#results').append(output);
 			});
+		});
+}
+
+// Create getOutput Function 
+function getOutput(item) {
+	// Declare Variables First
+	var videoId = item.id.videoId;
+	var title = item.snippet.title;
+	var description = item.snippet.description;
+	var thumbnails = item.snippet.thumbnails.high.url;
+	var channelTitle = item.snippet.channelTitle;
+	var videoDate = item.snippet.publishedAt;
+
+	var output = '<li>' +
+	'<div class="list-left">' + 
+	'<img src="' + thumbnails + '"/>' +
+	'</div>' + 
+	'<div class="list-right">' +
+	'<h3>' + title + '</h3>' +
+	'<small>By <span class="channelTitle">' + channelTitle + '</span> on ' + videoDate + '</small>' +
+	'<p>' + description + '</p>' +
+	'</div>' + 
+	'</li>'
+
+	return output;
 }
